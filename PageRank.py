348956537bf67e7a7615805be03443ac89daef50
Maxence Rencelot <rencelotm@oasis.uclouvain.be>
2020-05-04 11:02:27 +0200

    Use of np.matrix instead of np.array

--- PageRank.py
+++ PageRank.py
@@ -6,39 +6,41 @@ def page_rank_score(A, alpha=0.7):
     """
     Compute the page rank score from the matrix A and the teleportation's parameter alpha.
     :param A: The adjacency matrix of the graph
-    :param alpha: a parameter of teleportation
-    :return: A vector who contains the node's rank for the graph
+    :param alpha: a parameter of teleportation. By default it's worth 0.7
+    :return: A vector who contains the node's rank for the graph.
     """
     n = len(A)
-    H = probabilistic_matrix(A)
+    H = probabilistic_matrix(A, n)
     G = google_matrix(H, alpha, n)
     vector_result = power_method(G, n)
     return vector_result
 
 
-def probabilistic_matrix(A):
+def probabilistic_matrix(A, n):
     """
     This function compute the matrix of probability to go edge from another edge.
-    :param A: The adjacency matrix of a directed graph
-    :return: The matrix of probabilities
+    :param A: The adjacency matrix of a directed graph.
+    :param n: The len of the matrix A.
+    :return: The matrix of probabilities.
     """
-    for i in range(len(A)):
-        somme = sum(A[i, :])
+    for i in range(n):
+        somme = A[i, :].sum()
         if somme != 0:
             A[i, :] = A[i, :] / somme
     return A
 
 
-def dangling_node_vector(H):
+def dangling_node_vector(H, n):
     """
     Compute a dangling node vector.If a row of the probabilistic matrix is 0, so the same row of this vector is 1.
     Otherwise = 0.
     :param H: The probabilistic matrix of the starting matrix.
+    :param n: The len of the H matrix
     :return: return the dangling node vector of H.
     """
-    a = np.zeros((len(H), 1))
-    for i in range(len(H)):
-        if sum(H[i, :]) == 0:
+    a = np.zeros((n, 1))
+    for i in range(n):
+        if H[i, :].sum() == 0:
             a[i, 0] = 1
     return a
 
@@ -59,8 +61,9 @@ def stochastic_matrix(H, n):
     :param n: The len of the matrix.
     :return: The stochastic matrix from H.
     """
-    a = dangling_node_vector(H)
+    a = dangling_node_vector(H, n)
     e = vector_e(n)
+    print(e)
     S = H + a * ((1/n) * e.transpose())
     return S
 
@@ -72,7 +75,7 @@ def teleportation_matrix(n):
     :return: The teleportation matrix
     """
     e = vector_e(n)
-    E = (1/n) * (e * e.transpose())
+    E = (1/n) * (e.dot(e.transpose()))
     return E
 
 
@@ -106,9 +109,11 @@ def power_method(G, n):
 
 
 if __name__ == '__main__':
-    A = np.array([[0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 1, 0],
-                  [0, 0, 0, 0, 1, 1], [0, 0, 0, 1, 0, 1], [0, 0, 0, 1, 0, 0]], float)    # A est une matrice de tests
+    # A est une matrice test à utiliser avec α == 0.9
+    A = np.matrix([[0, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 1, 1], [0, 0, 0, 1, 0, 1], [0, 0, 0, 1, 0, 0]], float)
     with open('matrix.csv') as file:
-        matrix = np.array(list(csv.reader(file, dialect="excel", delimiter=',', lineterminator='\n')), float)
+        matrix = np.matrix(list(csv.reader(file, dialect="excel", delimiter=',', lineterminator='\n')), float)
     print(matrix)
     print(page_rank_score(matrix))
+
